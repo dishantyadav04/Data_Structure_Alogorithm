b59e481c976411e629d8fa5b0d051c9e9509ae5f
Dishant <dishant738264@gmail.com>
2025-03-05 17:35:50 +0530

    Day 1

---
+++ src/importantpatterns/Pattern_01.java
@@ -0,0 +1,24 @@
+package importantpatterns;
+
+import java.util.Scanner;
+
+public class Pattern_01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        //Square
+        for (int i =0; i < n; i++) {
+            for (int j =0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        //Rectangle
+        for (int i=1; i<=n; i++) {
+            for (int j=0; j<=n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/importantpatterns/Pattern_05.java
@@ -0,0 +1,22 @@
+package importantpatterns;
+
+import java.util.Scanner;
+
+public class Pattern_05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();;
+
+        for (int i=1; i<=n;i++) {
+            //space
+            for (int j=1; j<=n-i; j++) {
+                System.out.print(" ");
+            }
+            //star
+            for (int j=1; j<=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
